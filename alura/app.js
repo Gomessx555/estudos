b8da832f3a5ed82f567144eeef12b4d0e848dfde
Yuri Gomes <yuriciclista6@gmail.com>
2024-09-05 22:44:19 -0300

    aula 4 imersão alura - JavaScript

--- alura/app.js
+++ alura/app.js
@@ -1,1 +1,18 @@
-console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = ""
+    
+    for (let dado of dados) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}">Instagram</a>
+        </div>
+    `
+    }
+    
+    section.innerHTML = resultados
+}
+
